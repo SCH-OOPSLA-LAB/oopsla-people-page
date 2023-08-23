10957239d9fdd2b2b7c2451ed31f35af87bfaa0b
haZuny <gkwns5791@naver.com>
2023-08-23 17:40:13 +0900

    대충 감 잡음

---
+++ oppslaPeoplePage/main/urls.py
@@ -0,0 +1,11 @@
+import os
+from django.urls import path
+
+from . import views
+
+# 사용자 목록 폴더의 정보를 받아옴, 개개인의 리스트 생성
+currentPos = os.path.dirname(__file__)
+peopleList = os.listdir(currentPos+'\\peoples')
+pathList = [path(name, views.test, name=name) for name in peopleList]
+
+urlpatterns = pathList
--- oppslaPeoplePage/main/views.py
+++ oppslaPeoplePage/main/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def test(request):
+    name = (str(request).split("'")[1].split('/')[2])
+    print(name)
+
+    return render(request, "people-page.html", {'name':name})
