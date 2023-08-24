f4420a2c0eb77159c36659616ec4c039383a03db
haZuny <gkwns5791@naver.com>
2023-08-24 17:20:23 +0900

    모델 정의

--- oppslaPeoplePage/main/models.py
+++ oppslaPeoplePage/main/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class seminarPPT(models.Model):
+    people = models.CharField(max_length=10)
+    uploadDate = models.DateField(auto_created=True)
+    pptFile = models.FileField()
