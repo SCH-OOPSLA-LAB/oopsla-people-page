13908e52c863d9583d9711ffd4c6c3a2ea67f3e7
haZuny <gkwns5791@naver.com>
2023-08-24 16:33:44 +0900

    대충 뼈대 ㅇㅋ

--- oppslaPeoplePage/main/views.py
+++ oppslaPeoplePage/main/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+import os
+
+currentDir = str(os.path.abspath(__file__))[:-8]
 
 # Create your views here.
 def test(request):
-    name = (str(request).split("'")[1].split('/')[2])
-    print(name)
+    urlName = (str(request).split("'")[1].split('/')[2])
+    
+    # Get people info
+    peopleDir = currentDir + 'peoples\\' + urlName + "\\"
+    file = open(peopleDir + "profile.txt", "r", encoding='UTF-8')
+    
+    name = file.readline().split('//')[0].strip()
+    position = file.readline().split('//')[0].strip()
+    gitId = file.readline().split('//')[0].strip()
+    gitUrl = file.readline().split('//')[0].strip()
+    imgPath = 'assets/profileImg/' + file.readline().split('//')[0].strip()
 
-    return render(request, "people-page.html", {'name':name})
+    print(imgPath)
+    
+    return render(request, "people-page.html", {'name':name, 'position': position, 'gitId':gitId, 'gitUrl': gitUrl, 'imgPath': imgPath})
